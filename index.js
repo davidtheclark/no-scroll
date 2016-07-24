5262f57ca90809a0f2b6bcc87ce8ec1262eba432
Travis Arnold <ftntravis@gmail.com>
2016-07-24 15:16:08 -0700

    used fixed positioning on document to prevent scrolling on touch devices

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 (function(root) {
   var scrollbarSize;
+  var scrollTop;
 
   function getScrollbarSize() {
     if (typeof scrollbarSize !== 'undefined') return scrollbarSize;
 
     var doc = document.documentElement;
     var dummyScroller = document.createElement('div');
-    dummyScroller.setAttribute('style', 'width:99px;height:99px;'
-      + 'position:absolute;top:-9999px;overflow:scroll;');
+    dummyScroller.setAttribute('style', 'width:99px;height:99px;' + 'position:absolute;top:-9999px;overflow:scroll;');
     doc.appendChild(dummyScroller);
     scrollbarSize = dummyScroller.offsetWidth - dummyScroller.clientWidth;
     doc.removeChild(dummyScroller);
@@ -21,19 +21,25 @@
   function on(options) {
     if (typeof document === 'undefined') return;
     var doc = document.documentElement;
-    var rightPad = parseInt(getComputedStyle(doc)['padding-right'], 10);
-    doc.style.overflow = 'hidden';
+    scrollTop = window.pageYOffset;
     if (hasScrollbar()) {
-      rightPad += getScrollbarSize();
-      doc.style.paddingRight = rightPad + 'px';
+      doc.style.width = 'calc(100% - '+ getScrollbarSize() +'px)';
+    } else {
+      doc.style.width = '100%';
     }
+    doc.style.position = 'fixed';
+    doc.style.top = -scrollTop + 'px';
+    doc.style.overflow = 'hidden';
   }
 
   function off() {
     if (typeof document === 'undefined') return;
     var doc = document.documentElement;
+    doc.style.width = '';
+    doc.style.position = '';
+    doc.style.top = '';
     doc.style.overflow = '';
-    doc.style.paddingRight = '';
+    window.scroll(0, scrollTop);
   }
 
   var noScroll = {
@@ -43,8 +49,7 @@
 
   if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
     module.exports = noScroll;
-  }
-  else {
+  } else {
     root.noScroll = noScroll;
   }
 })(this);
